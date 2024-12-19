f3f1ee03d9740d2fb051ab2b6495af991db7b4e2
LaunchSpider <danylo.bezruchenko@gmail.com>
2024-12-19 11:07:38 +0200

    Added functionality

--- actions_py/count_until_client.py
+++ actions_py/count_until_client.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
-from rclpy.action.client import ClientGoalHandle
+from rclpy.action.client import ClientGoalHandle, GoalStatus
 from custom_interfaces.action import CountUntil
      
 class CountUntilClientNode(Node): 
@@ -21,16 +21,37 @@ class CountUntilClientNode(Node):
         
         #   Send a goal
         self.get_logger().info("Sending goal...")
-        self.count_until_client_.send_goal_async(goal).add_done_callback(self.goal_response_callback)
+        self.count_until_client_.send_goal_async(goal, feedback_callback=self.goal_feedback_callback).add_done_callback(self.goal_response_callback)
+        
+        #self.timer_ = self.create_timer(2.0, self.cancel_goal)
+    
+    def cancel_goal(self):
+        self.get_logger().info("Sending a cancel request...")
+        self.goal_handle_.cancel_goal_async()
+        self.timer_.cancel()
         
     def goal_response_callback(self, future):
         self.goal_handle_ : ClientGoalHandle = future.result()
         if self.goal_handle_.accepted:
+            self.get_logger().info("Goal got accepted!")
             self.goal_handle_.get_result_async().add_done_callback(self.goal_result_callback)
+        else:
+            self.get_logger().warn("Goal got rejected")
             
     def goal_result_callback(self, future):
+        status = future.result().status
         result = future.result().result
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info("Success")
+        elif status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().warn("Aborted")
+        elif status == GoalStatus.STATUS_CANCELED:
+            self.get_logger().error("Cancelled")
         self.get_logger().info("Result: " + str(result.reached_number))
+        
+    def goal_feedback_callback(self, feedback_msg):
+        number = feedback_msg.feedback.current_number
+        self.get_logger().info("Got feedback: " + str(number))
      
 def main(args=None):
     rclpy.init(args=args)
